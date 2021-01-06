9214637662a70f51661115c1172b1d806f381583
alestor123 <alestoraldous@gmail.com>
2021-01-06 21:06:16 +0530

    added main code

--- cli.js
+++ cli.js
@@ -1,2 +1,7 @@
 #!/usr/bin/env node
 
+var isitup = require('./App');
+(async () => {
+if(await isitup(process.argv[2] || 'github.com')) console.log('yeah its up you should check your connection')
+else console.log('Nope its down. And its gone it didnt do well its gone poof')
+})();
